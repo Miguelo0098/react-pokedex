134f1cf29610a687af5fc7a471e640c52f56a081
Miguel Ángel Ramírez Ruiz <mugui098@gmail.com>
2023-01-30 20:52:04 +0100

    feat: improve 404 page design

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,15 @@
-import * as React from "react";
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
 export interface INotFoundPageProps {}
 
 export function NotFoundPage(props: INotFoundPageProps) {
-  return <div>404 not found :c</div>;
+  return (
+    <Box component={"section"}>
+      <Typography variant="h1">{"404"}</Typography>
+      <Typography variant="h2" color={"primary"}>
+        {"Page not found"}
+      </Typography>
+    </Box>
+  );
 }
